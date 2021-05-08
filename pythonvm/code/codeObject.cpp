a70134381058d35eb7d9124179681bf432ed0a72
左亦 <910181598@qq.com>
2021-05-08 18:21:17 +0800

    pythonvm

---
+++ pythonvm/code/codeObject.cpp
@@ -0,0 +1,62 @@
+#include "code/codeObject.hpp"
+#include "object/hiString.hpp"
+#include "memory/heap.hpp"
+#include "memory/oopClosure.hpp"
+
+CodeKlass* CodeKlass::instance = NULL;
+
+CodeKlass* CodeKlass::get_instance() {
+    if (instance == NULL) {
+        instance = new CodeKlass();
+    }
+
+    return instance;
+}
+
+CodeKlass::CodeKlass() {
+    set_name(new HiString("code"));
+    add_super(ObjectKlass::get_instance());
+    HiTypeObject* dict_type_obj = new HiTypeObject();
+    set_type_object(dict_type_obj);
+}
+
+void CodeKlass::oops_do(OopClosure* f, HiObject* obj) {
+    CodeObject* co = (CodeObject*) obj;
+    assert(co && co->klass() == (Klass*)this);
+
+    f->do_oop((HiObject**)&co->_bytecodes);
+    f->do_array_list(&co->_names);
+    f->do_array_list(&co->_consts);
+    f->do_array_list(&co->_var_names);
+    f->do_array_list(&co->_free_vars);
+    f->do_array_list(&co->_cell_vars);
+    f->do_oop((HiObject**)&co->_co_name);
+    f->do_oop((HiObject**)&co->_file_name);
+    f->do_oop((HiObject**)&co->_notable);
+}
+
+size_t CodeKlass::size() {
+    return sizeof(CodeObject);
+}
+
+CodeObject::CodeObject(int argcount, int nlocals, int stacksize, int flag, HiString* bytecodes,
+    ArrayList<HiObject*>* consts, ArrayList<HiObject*>* names, ArrayList<HiObject*>* varnames, 
+    ArrayList<HiObject*>* freevars, ArrayList<HiObject*>* cellvars,
+    HiString* file_name, HiString* co_name, int lineno, HiString* notable):
+        _argcount(argcount),
+        _nlocals(nlocals),
+        _stack_size(stacksize),
+        _flag(flag),
+        _bytecodes(bytecodes),
+        _names(names),
+        _consts(consts),
+        _var_names(varnames),
+        _free_vars(freevars),
+        _cell_vars(cellvars),
+        _co_name(co_name),
+        _file_name(file_name),
+        _lineno(lineno),
+        _notable(notable){
+    set_klass(CodeKlass::get_instance());
+}
+
---
+++ pythonvm/runtime/functionObject.hpp
@@ -0,0 +1,132 @@
+#ifndef HI_FUNCTION_OBJECT_HPP
+#define HI_FUNCTION_OBJECT_HPP
+
+#include "code/codeObject.hpp"
+#include "object/klass.hpp"
+
+class HiDict;
+
+class FunctionKlass : public Klass {
+private:
+    FunctionKlass();
+    static FunctionKlass* instance;
+
+public:
+    static FunctionKlass* get_instance();
+
+    virtual void print(HiObject* obj);
+
+    virtual size_t size();
+    virtual void oops_do(OopClosure* f, HiObject* obj);
+};
+
+typedef HiObject* (*NativeFuncPointer)(ObjList args);
+
+class FunctionObject : public HiObject {
+friend class FunctionKlass;
+friend class NativeFunctionKlass;
+friend class FrameObject;
+
+private:
+    CodeObject* _func_code;
+    HiString*   _func_name;
+    HiDict*     _globals;
+    ObjList     _defaults;
+    HiList*     _closure;
+
+    NativeFuncPointer _native_func;
+
+    unsigned int _flags;
+
+public:
+    enum CO_FLAGS {
+        CO_VARARGS = 0x4,
+        CO_VARKEYWORDS = 0x8,
+        CO_GENERATOR = 0x20,
+    };
+
+    FunctionObject(HiObject* code_object);
+
+    FunctionObject(NativeFuncPointer nfp);
+
+    FunctionObject(Klass* klass) {
+        _func_code = NULL;
+        _func_name = NULL;
+        _flags     = 0;
+        _globals   = NULL;
+        _defaults  = NULL;
+
+        set_klass(klass);
+    }
+    
+    HiString*  func_name()   { return _func_name; }
+    int  flags()             { return _flags; }
+
+    HiDict* globals()        { return _globals; }
+    void set_globals(HiDict* x) { _globals = x; }
+
+    void set_default(ObjList defaults);
+    ObjList defaults()       { return _defaults; }
+
+    void set_closure(HiList* x) { _closure = x; }
+    HiList* closure()        { return _closure; }
+
+    HiObject*  call(ObjList args);
+};
+
+// Method objects.
+class MethodKlass : public Klass {
+private:
+    MethodKlass();
+    static MethodKlass* instance;
+
+public:
+    static MethodKlass* get_instance();
+    virtual size_t size();
+    virtual void oops_do(OopClosure* f, HiObject* obj);
+};
+
+class MethodObject : public HiObject {
+friend class MethodKlass;
+
+private:
+    HiObject* _owner;
+    FunctionObject* _func;
+
+public:
+    MethodObject(FunctionObject* func) : _owner(NULL), _func(func) {
+        set_klass(MethodKlass::get_instance());
+    }
+
+    MethodObject(FunctionObject* func, HiObject* owner) : _owner(owner), _func(func) {
+        set_klass(MethodKlass::get_instance());
+    }
+
+    void set_owner(HiObject * x)   { _owner = x; }
+    HiObject* owner()              { return _owner; }
+    FunctionObject* func()         { return _func; }
+
+    static bool is_function(HiObject* x);
+    static bool is_yield_function(HiObject* x);
+};
+
+class NativeFunctionKlass : public Klass {
+private:
+    NativeFunctionKlass();
+    static NativeFunctionKlass* instance;
+
+public:
+    static NativeFunctionKlass* get_instance();
+
+    virtual size_t size();
+    virtual void oops_do(OopClosure* f, HiObject* obj);
+};
+
+HiObject* len(ObjList args);
+HiObject* iter(ObjList args);
+HiObject* type_of(ObjList args);
+HiObject* isinstance(ObjList args);
+HiObject* builtin_super(ObjList args);
+HiObject* sysgc(ObjList args);
+
+#endif
