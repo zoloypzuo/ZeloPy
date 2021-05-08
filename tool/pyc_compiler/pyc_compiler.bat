5ca781ea0428602aebbc9516a00823384e31754d
左亦 <910181598@qq.com>
2021-05-08 18:41:06 +0800

    Create pyc_compiler.bat

---
+++ tool/pyc_compiler/pyc_compiler.bat
@@ -0,0 +1,29 @@
+"C:\Python27\python.exe" -m compileall py\hello.py
+move py\hello.pyc pyc
+"C:\Python27\python.exe" -m compileall py\test_if.py
+move py\test_if.pyc pyc
+"C:\Python27\python.exe" -m compileall py\test_var.py
+move py\test_var.pyc pyc
+"C:\Python27\python.exe" -m compileall py\test_while.py
+move py\test_while.pyc pyc
+"C:\Python27\python.exe" -m compileall py\simple_while.py
+move py\simple_while.pyc pyc
+"C:\Python27\python.exe" -m compileall py\test_param.py
+move py\test_param.pyc pyc
+"C:\Python27\python.exe" -m compileall py\test_default.py
+move py\test_default.pyc pyc
+"C:\Python27\python.exe" -m compileall py\test_native.py
+move py\test_native.pyc pyc
+"C:\Python27\python.exe" -m compileall py\test_method.py
+move py\test_method.pyc pyc
+"C:\Python27\python.exe" -m compileall py\test_class.py
+move py\test_class.pyc pyc
+"C:\Python27\python.exe" -m compileall py\t_6_1.py
+move py\t_6_1.pyc pyc
+"C:\Python27\python.exe" -m compileall py\t_6_1_1.py
+move py\t_6_1_1.pyc pyc
+"C:\Python27\python.exe" -m compileall py\t_8_1.py
+move py\t_8_1.pyc pyc
+"C:\Python27\python.exe" -m compileall py\t_8_2.py
+move py\t_8_2.pyc pyc
+pause
